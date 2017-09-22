86b864780d055dd823818fc0d1bc0b3b30999cdb
leonardorossetti <leoo.rossetti@gmail.com>
2017-09-21 22:14:39 -0300

    Até aula 46 - Criacao de menus e rotas

---
+++ My/project_react/frontend/src/main/app.jsx
@@ -0,0 +1,14 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+import Menu from '../template/menu'
+import Routes from './routes'
+
+//o que está entre parenteses é uma expressao que está sendo retornada
+export default props => (
+    <div className='container'>
+        <Menu />
+        <Routes />
+    </div>
+)
