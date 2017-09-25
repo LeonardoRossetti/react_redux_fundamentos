e14cdc124171fa949daa0bf2765e69a5c0a94301
leonardorossetti <leoo.rossetti@gmail.com>
2017-09-25 20:52:05 -0300

    alteracoes aula 53 e 54

--- My/project_react/frontend/src/todo/todo.jsx
+++ My/project_react/frontend/src/todo/todo.jsx
@@ -4,11 +4,37 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { description: '', list: [] }
+
+        /**
+         * No React o objeto 'this' muda conforme 
+         * quem chamou a função e não como nas outras linguagens.
+         * Para que, independente de quem chame essa função
+         * o 'this' seja sempre o componente atual, ou
+         * nesse caso, a classe Todo.
+         */
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value })
+    }
+    
+    handleAdd() {
+        console.log(this.state.description)
+    }
+    
     render(){
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm />
+                <TodoForm description = {this.state.description}
+                 handleAdd={this.handleAdd}
+                 handleChange={this.handleChange} />
                 <TodoList />
             </div>
         )
--- My/project_react/frontend/src/todo/todoForm.jsx
+++ My/project_react/frontend/src/todo/todoForm.jsx
@@ -7,11 +7,14 @@ export default props => (
         
         <Grid cols='12 9 10'>
             <input className="form-control" id="description"
-                placeholder="Adicione uma tarefa"></input>
+                placeholder="Adicione uma tarefa"
+                value={props.description}
+                onChange={props.handleChange}></input>
         </Grid>
 
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus' />
+            <IconButton style='primary' icon='plus' 
+                onClick={props.handleAdd} />
         </Grid>
     </div>
 )
